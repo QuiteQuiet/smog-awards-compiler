1f81de5ee4ec99d628ab963e7132e210aeb41be5
Quite Quiet <femtontusen@gmail.com>
2023-01-29 17:10:16 +0100

    Replaced deprecated requests-promise with needle

--- xenforo.js
+++ xenforo.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const rp = require('request-promise').defaults({jar: true});
+const needle = require('needle');
 const cheerio = require('cheerio');
 
 class AuthenticationError extends Error {
@@ -13,76 +13,71 @@ class AuthenticationError extends Error {
 
 class XenforoLoginSession {
     constructor(username, password) {
-        this.xenforosite = '';
+        this.site = '';
         this.username = username;
         this.password = password;
+
         // Default stuff used for all requests that's going to be done
-        this.request = {
-            headers: {
-               'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-               'accept-encoding': 'gzip, deflate, br',
-               'connection': 'keep-alive',
-            },
-            gzip: true,
+        this.headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'accept-encoding': 'gzip, deflate, br',
+            'connection': 'keep-alive',
+            'cookies': {},
+        };
+    }
+
+    get url() { return this.site; }
+
+    setCookies(cookies) {
+        if (!cookies) return;
+        for (const [name, value] of Object.entries(cookies)) {
+            this.headers['cookies'][name] = value;
         }
     }
 
-    get url() { return this.xenforosite; }
-
-    start(xenforosite) {
+    start(site) {
         // This is required because Javascript
         let self = this;
         return new Promise((resolve, reject) => {
-            if (self.xenforosite) reject(new AuthenticationError('Already logged in'));
+            if (self.site) reject(new AuthenticationError('Already logged in'));
 
-            let tokenRequest = Object.assign({}, this.request);;
-            tokenRequest.resolveWithFullResponse = true;
-            tokenRequest.simple = false;
-            tokenRequest.uri = xenforosite;
-
-            rp.get(tokenRequest)
+            needle('get', site)
             .then(response => {
+                self.setCookies(response.cookies);
                 let $ = cheerio.load(response.body);
                 let xfToken = $('input[name="_xfToken"]').val();
 
                 // Now we can send a login request with the provided username and password
-                let loginRequest = Object.assign({}, self.request);
-                loginRequest.resolveWithFullResponse = true;
-                loginRequest.simple = false;
-                loginRequest.uri = `${xenforosite}/login/login`;
-
-                loginRequest.formData = {
-                    _xfRedirect: xenforosite,
+                let request = {
+                    _xfRedirect: site,
                     _xfToken: xfToken,
                     login: self.username,
                     password: self.password,
                 };
 
-                return rp.post(loginRequest);
+                return needle('post', `${site}login/login`, request, this.headers);
             })
             .then(response => {
                 // If the response is 303 here we should have been properly logged in
                 if (response.statusCode !== 303) {
-                    reject(new AuthenticationError(`Error while logging into ${xenforosite}!`));
+                    reject(new AuthenticationError(`Error while logging into ${site}!`));
                 } else {
-                    self.xenforosite = xenforosite;
-                    resolve(`Finished logging in to ${xenforosite}`);
+                    self.setCookies(response.cookies);
+                    self.site = site;
+                    resolve(`Finished logging in to ${site}`);
                 }
             })
-            .catch(err => console.error);
+            .catch(err => reject(err));
         });
     }
 
     open(url) {
         let self = this;
-        if (!url.startsWith(this.xenforosite)) {
-            url = `${this.xenforosite}${url}`;
+        if (!url.startsWith(this.site)) {
+            url = `${this.site}${url}`;
         }
         return new Promise((resolve, reject) => {
-            let request = Object.assign({}, self.request);
-            request.uri = url;
-
-            rp.get(request)
+            needle('get', url, null, self.headers)
             .then(response => resolve(response))
             .catch(err => reject(err));
         });
